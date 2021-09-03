1344031c4f6d3b96e7297cb7fe9e4888d75078cc
vishal <vishal.k@simplifycv.com>
2021-09-03 20:53:34 +0530

    working code with remote server

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -141,119 +141,135 @@ import Peer from "peerjs";
 export default {
   name: "Home",
   components: {},
-  mounted: function(){
+  mounted: function () {
     var peer_id;
     var username;
     var conn;
 
     var peer = new Peer({
-        host: "localhost",
-        port: 9000,
-        path: '/peerjs',
-        debug: 3,
-        config: {
-            'iceServers': [
-                { url: 'stun:stun1.l.google.com:19302' },
-                {
-                    url: 'turn:numb.viagenie.ca',
-                    credential: 'muazkh',
-                    username: 'webrtc@live.com'
-                }
-            ]
-        }
+      host: "157.245.101.140",
+      port: 9000,
+      path: "/peerjs",
+      debug: 3,
+      config: {
+        iceServers: [
+          { url: "stun:stun1.l.google.com:19302" },
+          {
+            url: "turn:numb.viagenie.ca",
+            credential: "muazkh",
+            username: "webrtc@live.com",
+          },
+        ],
+      },
     });
     console.log(peer);
 
-    peer.on('open', function () {
-        document.getElementById("peer-id-label").innerHTML = peer.id;
-    });
-    
-    peer.on('connection', function (connection) {
-        conn = connection;
-        peer_id = connection.peer;
-
-        // Use the handleMessage to callback when a message comes in
-        conn.on('data', handleMessage);
-
-        // Hide peer_id field and set the incoming peer id as value
-        document.getElementById("peer_id").className += " hidden";
-        document.getElementById("peer_id").value = peer_id;
-        document.getElementById("connected_peer").innerHTML = connection.metadata.username;
+    peer.on("open", function () {
+      document.getElementById("peer-id-label").innerHTML = peer.id;
     });
 
-    peer.on('error', function(err){
-        alert("An error ocurred with peer: " + err);
-        console.error(err);
+    peer.on("connection", function (connection) {
+      conn = connection;
+      peer_id = connection.peer;
+
+      // Use the handleMessage to callback when a message comes in
+      conn.on("data", handleMessage);
+
+      // Hide peer_id field and set the incoming peer id as value
+      document.getElementById("peer_id").className += " hidden";
+      document.getElementById("peer_id").value = peer_id;
+      document.getElementById("connected_peer").innerHTML =
+        connection.metadata.username;
     });
 
-    peer.on('call', function (call) {
-        var acceptsCall = confirm("Videocall incoming, do you want to accept it ?");
+    peer.on("error", function (err) {
+      alert("An error ocurred with peer: " + err);
+      console.error(err);
+    });
 
-        if(acceptsCall){
-            // Answer the call with your own video/audio stream
-            call.answer(window.localStream);
+    peer.on("call", function (call) {
+      var acceptsCall = confirm(
+        "Videocall incoming, do you want to accept it ?"
+      );
 
-            // Receive data
-            call.on('stream', function (stream) {
-                // Store a global reference of the other user stream
-                window.peer_stream = stream;
-                // Display the stream of the other user in the peer-camera video element !
-                onReceiveStream(stream, 'peer-camera');
-            });
+      if (acceptsCall) {
+        // Answer the call with your own video/audio stream
+        call.answer(window.localStream);
 
-            // Handle when the call finishes
-            call.on('close', function(){
-                alert("The videocall has finished");
-            });
+        // Receive data
+        call.on("stream", function (stream) {
+          // Store a global reference of the other user stream
+          window.peer_stream = stream;
+          // Display the stream of the other user in the peer-camera video element !
+          onReceiveStream(stream, "peer-camera");
+        });
 
-            // use call.close() to finish a call
-        }else{
-            console.log("Call denied !");
-        }
+        // Handle when the call finishes
+        call.on("close", function () {
+          alert("The videocall has finished");
+        });
+
+        // use call.close() to finish a call
+      } else {
+        console.log("Call denied !");
+      }
     });
 
     function requestLocalVideo(callbacks) {
-        // Monkeypatch for crossbrowser geusermedia
-        navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+      // Monkeypatch for crossbrowser geusermedia
+      navigator.getUserMedia =
+        navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia;
 
-        // Request audio an video
-        navigator.getUserMedia({ audio: true, video: true }, callbacks.success , callbacks.error);
+      // Request audio an video
+      navigator.getUserMedia(
+        { audio: true, video: true },
+        callbacks.success,
+        callbacks.error
+      );
     }
 
     function onReceiveStream(stream, element_id) {
-        // Retrieve the video element according to the desired
-        var video = document.getElementById(element_id);
-        // Set the given stream as the video source
-        video.srcObject = stream;
+      // Retrieve the video element according to the desired
+      var video = document.getElementById(element_id);
+      // Set the given stream as the video source
+      video.srcObject = stream;
 
-        // Store a global reference of the stream
-        window.peer_stream = stream;
+      // Store a global reference of the stream
+      window.peer_stream = stream;
     }
 
     function handleMessage(data) {
-        var orientation = "text-left";
+      var orientation = "text-left";
 
-        // If the message is yours, set text to right !
-        if(data.from == username){
-            orientation = "text-right"
-        }
+      // If the message is yours, set text to right !
+      if (data.from == username) {
+        orientation = "text-right";
+      }
 
-        var messageHTML =  '<a href="javascript:void(0);" class="list-group-item' + orientation + '">';
-                messageHTML += '<h4 class="list-group-item-heading">'+ data.from +'</h4>';
-                messageHTML += '<p class="list-group-item-text">'+ data.text +'</p>';
-            messageHTML += '</a>';
+      var messageHTML =
+        '<a href="javascript:void(0);" class="list-group-item' +
+        orientation +
+        '">';
+      messageHTML +=
+        '<h4 class="list-group-item-heading">' + data.from + "</h4>";
+      messageHTML += '<p class="list-group-item-text">' + data.text + "</p>";
+      messageHTML += "</a>";
 
-        document.getElementById("messages").innerHTML += messageHTML;
+      document.getElementById("messages").innerHTML += messageHTML;
     }
 
-    document.getElementById("send-message").addEventListener("click", function(){
+    document.getElementById("send-message").addEventListener(
+      "click",
+      function () {
         // Get the text to send
         var text = document.getElementById("message").value;
 
         // Prepare the data to send
         var data = {
-            from: username,
-            text: text
+          from: username,
+          text: text,
         };
 
         // Send the message with Peer
@@ -263,55 +279,63 @@ export default {
         handleMessage(data);
 
         document.getElementById("message").value = "";
-    }, false);
+      },
+      false
+    );
 
-    document.getElementById("call").addEventListener("click", function(){
-        console.log('Calling to ' + peer_id);
+    document.getElementById("call").addEventListener(
+      "click",
+      function () {
+        console.log("Calling to " + peer_id);
         console.log(peer);
 
         var call = peer.call(peer_id, window.localStream);
 
-        call.on('stream', function (stream) {
-            window.peer_stream = stream;
+        call.on("stream", function (stream) {
+          window.peer_stream = stream;
 
-            onReceiveStream(stream, 'peer-camera');
+          onReceiveStream(stream, "peer-camera");
         });
-    }, false);
+      },
+      false
+    );
 
-    document.getElementById("connect-to-peer-btn").addEventListener("click", function(){
+    document.getElementById("connect-to-peer-btn").addEventListener(
+      "click",
+      function () {
         username = document.getElementById("name").value;
         peer_id = document.getElementById("peer_id").value;
 
         if (peer_id) {
-            conn = peer.connect(peer_id, {
-                metadata: {
-                    'username': username
-                }
-            });
+          conn = peer.connect(peer_id, {
+            metadata: {
+              username: username,
+            },
+          });
 
-            conn.on('data', handleMessage);
-        }else{
-            alert("You need to provide a peer to connect with !");
-            return false;
+          conn.on("data", handleMessage);
+        } else {
+          alert("You need to provide a peer to connect with !");
+          return false;
         }
 
         document.getElementById("chat").className = "";
         document.getElementById("connection-form").className += " hidden";
-    }, false);
+      },
+      false
+    );
 
     requestLocalVideo({
-        success: function(stream){
-            window.localStream = stream;
-            onReceiveStream(stream, 'my-camera');
-        },
-        error: function(err){
-            alert("Cannot get access to your camera and video !");
-            console.error(err);
-        }
+      success: function (stream) {
+        window.localStream = stream;
+        onReceiveStream(stream, "my-camera");
+      },
+      error: function (err) {
+        alert("Cannot get access to your camera and video !");
+        console.error(err);
+      },
     });
-}
-
-  
+  },
 };
 </script>
 <style></style>
